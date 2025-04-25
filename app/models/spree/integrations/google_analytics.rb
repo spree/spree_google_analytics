127f5d5d0164f62993b0a4f6c5d9c78659953be8
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-04-25 17:28:44 +0200

    Remove the API secret from the GA4 integration

--- app/models/spree/integrations/google_analytics.rb
+++ app/models/spree/integrations/google_analytics.rb
@@ -2,9 +2,8 @@ module Spree
   module Integrations
     class GoogleAnalytics < Spree::Integration
       preference :measurement_id, :string
-      preference :api_secret, :password
 
-      validates :preferred_measurement_id, :preferred_api_secret, presence: true
+      validates :preferred_measurement_id, presence: true
 
       def self.integration_group
         'analytics'
