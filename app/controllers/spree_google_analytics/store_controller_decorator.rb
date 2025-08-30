12c16d77a51ea53919511d9113c343fad2af6b18
Damian Legawiec <damian@getvendo.com>
2025-08-30 15:20:21 +0200

    Include `BaseHelper` in devise controllers

--- app/controllers/spree_google_analytics/store_controller_decorator.rb
+++ app/controllers/spree_google_analytics/store_controller_decorator.rb
@@ -7,3 +7,14 @@ module SpreeGoogleAnalytics
 end
 
 Spree::StoreController.prepend(SpreeGoogleAnalytics::StoreControllerDecorator) if defined?(Spree::StoreController)
+
+# include in Devise controllers
+if defined?(Spree::UserSessionsController)
+  Spree::UserSessionsController.prepend(SpreeGoogleAnalytics::UserSessionsControllerDecorator)
+end
+if defined?(Spree::UserRegistrationsController)
+  Spree::UserRegistrationsController.prepend(SpreeGoogleAnalytics::UserRegistrationsControllerDecorator)
+end
+if defined?(Spree::UserPasswordsController)
+  Spree::UserPasswordsController.prepend(SpreeGoogleAnalytics::UserPasswordsControllerDecorator)
+end
